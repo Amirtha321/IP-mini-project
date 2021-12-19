cefb6b9a14dd3e37a2112bd5239c6e6b2a99e1bd
Akhash16 <akhash.xs@gmail.com>
2021-12-19 23:08:34 +0530

    .

--- register.php
+++ register.php
@@ -39,19 +39,3 @@
 
 </html>
 
-<?php
-include 'config.php';
-session_start();
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-$pwd = $_POST["password"];
-
-$sql = "INSERT INTO customers(name, email, password) VALUES ('$name','$email','$pwd')";
-if ($con->query($sql) === TRUE) {
-    header('location: login.html');
-  } else {
-    echo "Error: " . $sql . "<br>" . $con->error;
-  }
-
-?>
